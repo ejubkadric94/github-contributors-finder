6e0408655a93e507ecb10aa31f55d7ef67a57fe7
Ejub Kadric <ejubkadric@gmail.com>
2023-02-15 17:30:48 +0100

    set lower debounce time

--- github-contributors-frontend/src/customHooks/useDebounce.ts
+++ github-contributors-frontend/src/customHooks/useDebounce.ts
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+
+const DEFAULT_DEBOUNCE_TIME = 300;
 
 function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay || DEFAULT_DEBOUNCE_TIME);
 
     return () => {
       clearTimeout(timer);
